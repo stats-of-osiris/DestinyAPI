0831dea6157b392f54dbcbdbf0917ba4c292acbf
JohnOfMars <johnofmars@users.noreply.github.com>
2016-05-25 17:11:59 -0400

    Added template report writing script & some cleanup

--- test.py
+++
@@ -1,9 +0,0 @@
-import json
-import destiny
-import requests
-import os
-
-headers = {'X-API-Key': os.environ['BUNGIE_NET_API_KEY']}
-r = requests.get('https://www.bungie.net/Platform/Destiny/2/'
-                 'Stats//JohnOfMars', headers=headers)
-print(json.dumps(r.json(), indent=4))
