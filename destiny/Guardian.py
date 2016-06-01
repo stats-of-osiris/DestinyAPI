3dbf4fb6267742ed3e35fb94b7a59c4cc1656800
yergi <yergi@users.noreply.github.com>
2016-06-01 13:12:57 -0400

    Revert "Merge remote-tracking branch 'refs/remotes/origin/model-change'"
    This reverts commit f183f992eb8f29545adbbd2055deb52ff1a6809b, reversing
    changes made to 42942e9161e843e922a893b248a1c6c039973cdb.

--- destiny/Guardian.py
+++ destiny/Guardian.py
@@ -4,9 +4,8 @@
 destiny.Guardian
 ~~~~~~~~~~~~~~~~
 
-    This class provides access to the
-        `GetDestinyAccountSummary`
-    endpoint of the Destiny API.
+This class provides access to the `GetDestinyAccountSummary` endpoint of the
+Destiny API.
 
 """
 from . import utils, constants
@@ -14,50 +13,40 @@ from . import utils, constants
 
 class Guardian(object):
     """
-    Create a Guardian from JSON data
+    Create Guardian object using JSON data from the
+    `GetDestinyAccountSummary` endpoint.
     :param data: JSON blob specific to this guardian
     """
     def __init__(self, data):
         self.data = data
-        if self.usingPlayerData():
-            self.last_played = self.get('characterBase.dateLastPlayed')
-            self.id = self.get('characterBase.characterId')
-            self.player_id = self.get('characterBase.membershipId')
-            self.player_name = None
-            self.console_id = self.get('characterBase.membershipType')
-            self.light_level = self.get('characterBase.powerLevel')
-            self.guardian_level = self.get('characterLevel')
-            self.g_class = constants.GUARDIAN_TYPE[self.get('characterBase.classHash')]
-            # self.gender = constants.GUARDIAN_GENDER[self.get('characterBase.genderHash')]
-            # self.race = constants.GUARDIAN_RACE[self.get('characterBase.raceHash')]
-        else:
-            self.last_played = None
-            self.id = self.get('characterId')
-            self.player_id = self.get('player.destinyUserInfo.membershipId')
-            self.player_name = self.get('player.destinyUserInfo.displayName')
-            self.console_id = self.get('player.destinyUserInfo.membershipType')
-            self.g_class = self.get('player.characterClass')
-            self.light_level = self.get('player.lightLevel')
-            self.guardian_level = self.get('player.characterLevel')
-            # self.gender = None
-            # self.race = None
+        self.guardian_id = self.get('characterBase.characterId')
+        self.account_id = self.get('characterBase.membershipId')
+        self.account_type = self.get('characterBase.membershipType')
+        self.type = constants.GUARDIAN_TYPE[
+            self.get('characterBase.classHash')
+        ]
+        self.gender = constants.GUARDIAN_GENDER[
+            self.get('characterBase.genderHash')
+        ]
+        self.race = constants.GUARDIAN_RACE[
+            self.get('characterBase.raceHash')
+        ]
+        self.light_level = self.get('characterBase.powerLevel')
+        self.char_level = self.get('characterLevel')
 
     @classmethod
-    def guardians_from_data(cls, data, player_name=None):
-        """
-        Create Guardian object using JSON data.
-        :param guardian_data: JSON blob for all guardians
-        """
+    def guardians_from_data(cls, guardian_data):
         guardians = {}
-        for d in data:
-            guardian = cls(d)
-            if player_name:
-                guardian.player_name = player_name
-            guardians[guardian.id] = guardian
+        for gd in guardian_data:
+            guardian = cls(gd)
+            guardians[guardian.guardian_id] = guardian
         return guardians
 
-    def usingPlayerData(self):
-        return self.get('characterBase', False)
+    def get(self, data_path):
+        """
+        Get the value from a dict entry by specifying a period-delimited string
+        :param data_path: period-delimited string defining path to wanted value
+        :return: value of specified key from underlying JSON object
+        """
+        return utils.crawl_data(self, data_path)
 
-    def get(self, data_path, throw_error=True):
-        return utils.crawl_data(self, data_path, throw_error)
--- destiny/__init__.py
+++ destiny/__init__.py
@@ -2,7 +2,6 @@
 # This is for easily exposing visible names so they can be called like via
 # destiny.CarnageReport() instead of destiny.CarnageReport.CarnageReport()
 
-from .Player import Player
-from .Game import Game
+from .Account import Account
 from .Guardian import Guardian
-from .ReportContext import ReportContext
+from .CarnageReport import CarnageReport, CarnagePlayers
